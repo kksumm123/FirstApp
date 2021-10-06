897db8b0867ddca70adea5aaa3ef34ffb308ca74
kksumm123 <kksumm123@gmail.com>
2021-10-06 20:34:50 +0900

    1. Start 함수 -> Awake로 변경 2. RewardedAdsButton 예제 구성

--- Assets/TestButtonManager.cs
+++ Assets/TestButtonManager.cs
@@ -7,7 +7,7 @@ using UnityEngine.UI;
 public class TestButtonManager : MonoBehaviour
 {
     public Button baseButton;
-    void Start()
+    void Awake()
     {
         InterstitialAdExample interstitialAdExample = FindObjectOfType<InterstitialAdExample>();
         AddButton("전면 광고 로드", () => interstitialAdExample.LoadAd());
@@ -17,7 +17,10 @@ public class TestButtonManager : MonoBehaviour
         bannerAdExample._loadBannerButton = AddButton("배너 로드", null);
         bannerAdExample._showBannerButton = AddButton("배너 보여주기", null);
         bannerAdExample._hideBannerButton = AddButton("배너 숨기기", null);
-        
+
+        RewardedAdsButton rewardedAdsButton = FindObjectOfType<RewardedAdsButton>();
+        AddButton("리워드 광고 로드", () => rewardedAdsButton.LoadAd());
+        rewardedAdsButton._showAdButton = AddButton("리워드 광고 보여주기", null);
         
         baseButton.gameObject.SetActive(false);
     }
