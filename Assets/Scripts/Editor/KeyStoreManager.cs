44aa799c037d979efba73445bbcb290a3f796f65
kksumm123 <kksumm123@gmail.com>
2021-10-08 17:49:07 +0900

    빌드 패스워드 자동입력 스크립트

---
+++ Assets/Scripts/Editor/KeyStoreManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System;
+using UnityEditor.Build.Reporting;
+using UnityEditor.Build;
+
+class MyCustomBuildProcessor : IPreprocessBuildWithReport
+{
+    public int callbackOrder { get { return 0; } }
+    public void OnPreprocessBuild(BuildReport report)
+    {
+        Debug.Log("MyCustomBuildProcessor.OnPreprocessBuild for target " + report.summary.platform + " at path " + report.summary.outputPath);
+        SetKeystore();
+    }
+
+    public static void SetKeystore()
+    {
+        string keystorePath = $"{Application.dataPath}/../KeyStore/FirstAPP.keystore";
+        string keyaliasName = "1324224";
+        string pass = "1324224";
+
+        PlayerSettings.Android.keystoreName = keystorePath;
+        PlayerSettings.Android.keyaliasName = keyaliasName;
+        PlayerSettings.Android.keyaliasPass = pass;
+        PlayerSettings.Android.keystorePass = pass;
+    }
+}
